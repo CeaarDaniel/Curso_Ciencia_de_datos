67324d3ec342390069253298f47500916bd79375
CeaarDaniel <73078916+CeaarDaniel@users.noreply.github.com>
2023-03-03 20:37:56 -0600

    confi

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,1 @@
 print('Hola mundo')
-num1= 5
-num2 = -3
-def sum(numero1, numerto2):
-    total= numero1 + numerto2
-    return total
-
-resultado = sum(num1, num2)
-print('El resultado es: ', resultado)
